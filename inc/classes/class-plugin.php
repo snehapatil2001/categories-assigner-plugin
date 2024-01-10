c37772251daa8a689ac5c561f067f5dc3ff01b62
Sneha <snehapatildeepali@gmail.com>
2024-01-10 09:42:33 +0530

    Feat: Count of images in post.

--- inc/classes/class-plugin.php
+++ inc/classes/class-plugin.php
@@ -2,9 +2,13 @@
 
 namespace PMC_Plugin\Inc\Classes\Class_Plugin;
 
+/**
+ * Plugin
+ */
 class Plugin {
-    	
-    /**
+
+
+	/**
 	 * Instance of class.
 	 *
 	 * @var $instance
@@ -32,6 +36,29 @@ class Plugin {
 	 * This will register commands for assigning categories.
 	 */
 	protected function __construct() {
-        \WP_CLI::add_command( 'assign-category', 'PMC_Plugin\Inc\Classes\Class_Assign_Category\AssignCategory' );
+		\WP_CLI::add_command( 'assign-category', 'PMC_Plugin\Inc\Classes\Class_Assign_Category\Assign_Category' );
+	}
+
+	/**
+	 * Count images from post content and featured image.
+	 *
+	 * @param int $post_id Post ID.
+	 * @return int Image count.
+	 */
+	public static function count_images( $post_id ) {
+		// Get featured image count.
+		$featured_image_count = has_post_thumbnail( $post_id ) ? 1 : 0;
+
+		// Get post content.
+		$post_content = get_post_field( 'post_content', $post_id );
+
+		// Count images in post content.
+		preg_match_all( '/<!--\s*wp:image[^>]*-->.*?<!--\s*\/wp:image\s*-->/s', $post_content, $matches );
+		$content_image_count = count( $matches[0] );
+
+		// Total image count.
+		$total_image_count = $featured_image_count + $content_image_count;
+
+		return $total_image_count;
 	}
 }
