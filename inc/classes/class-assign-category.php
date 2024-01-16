d59fe51048b20d9540025e183a2ef13ac487ef68
Sneha <snehapatildeepali@gmail.com>
2024-01-16 16:38:14 +0530

    Fix : Add check for shortcode and formatting.

--- inc/classes/class-assign-category.php
+++ inc/classes/class-assign-category.php
@@ -4,91 +4,89 @@ namespace PMC_Plugin\Inc\Classes\Class_Assign_Category;
 
 use PMC_Plugin\Inc\Classes\Class_Plugin\Plugin;
 
-if (class_exists('WPCOM_VIP_CLI_Command')) {
-    /**
-     * Assign_Category
-     */
-    class Assign_Category extends \WPCOM_VIP_CLI_Command
-    {
-        /**
-         * Assign category to all posts.
-         *
-         * ## EXAMPLES
-         *
-         * wp assign-category update_post
-         *
-         * @when after_wp_load
-         */
-        public function update_post(array $args, array $assoc_args): void
-        {
-            // Disable term counting, Elasticsearch indexing, and PushPress.
-            $this->start_bulk_operation();
+if ( class_exists( 'WPCOM_VIP_CLI_Command' ) ) {
+	/**
+	 * Assign_Category
+	 */
+	class Assign_Category extends \WPCOM_VIP_CLI_Command {
 
-            $posts_per_page = 100;
-            $paged = 1;
-            $count = 0;
+		/**
+		 * Assign category to all posts.
+		 *
+		 * ## EXAMPLES
+		 *
+		 * wp assign-category update_post
+		 *
+		 * @when after_wp_load
+		 */
+		public function update_post( array $args, array $assoc_args ): void {
+			// Disable term counting, Elasticsearch indexing, and PushPress.
+			$this->start_bulk_operation();
 
-            // Check if parent category exists, and create it if not.
+			$posts_per_page = 100;
+			$paged          = 1;
+			$count          = 0;
+
+			// Check if parent category exists, and create it if not.
             // phpcs:ignore
             $parent_category = term_exists('pmc', 'category');
 
-            if (0 === $parent_category || null === $parent_category) {
-                // 'pmc' category doesn't exist, create it.
-                $parent_category = wp_insert_term('pmc', 'category');
-                \WP_CLI::success('Parent category "pmc" created.');
-            }
+			if ( 0 === $parent_category || null === $parent_category ) {
+				// 'pmc' category doesn't exist, create it.
+				$parent_category = wp_insert_term( 'pmc', 'category' );
+				\WP_CLI::success( 'Parent category "pmc" created.' );
+			}
 
-            // Check if child category exists, and create it if not.
+			// Check if child category exists, and create it if not.
             // phpcs:ignore
             $child_category = term_exists('rollingstone', 'category');
 
-            if (0 === $child_category || null === $child_category) {
-                // 'rollingstone' category doesn't exist, create it under the 'pmc' parent.
-                $child_category = wp_insert_term('rollingstone', 'category', array('parent' => $parent_category['term_id']));
-                \WP_CLI::success('Child category "rollingstone" created under "pmc".');
-            }
+			if ( 0 === $child_category || null === $child_category ) {
+				// 'rollingstone' category doesn't exist, create it under the 'pmc' parent.
+				$child_category = wp_insert_term( 'rollingstone', 'category', array( 'parent' => $parent_category['term_id'] ) );
+				\WP_CLI::success( 'Child category "rollingstone" created under "pmc".' );
+			}
 
-            do {
-                $query_args = array(
-                    'posts_per_page'   => $posts_per_page,
-                    'paged'            => $paged,
-                    'fields'           => 'ids',
-                );
+			do {
+				$query_args = array(
+					'posts_per_page' => $posts_per_page,
+					'paged'          => $paged,
+					'fields'         => 'ids',
+				);
 
-                $posts_query = new \WP_Query($query_args);
-                $post_ids = $posts_query->posts;
+				$posts_query = new \WP_Query( $query_args );
+				$post_ids    = $posts_query->posts;
 
-                // Assign child category 'rollingstone' to all posts.
-                foreach ($post_ids as $post_id) {
-                    $categories = wp_get_post_categories($post_id, array('fields' => 'slugs'));
+				// Assign child category 'rollingstone' to all posts.
+				foreach ( $post_ids as $post_id ) {
+					$categories = wp_get_post_categories( $post_id, array( 'fields' => 'slugs' ) );
 
-                    // Count images from post content and featured image.
-                    $image_count = Plugin::count_images($post_id);
+					// Count images from post content and featured image.
+					$image_count = Plugin::count_images( $post_id );
 
-                    // Add meta with the image count.
-                    update_post_meta($post_id, '_pmc_image_counts', $image_count);
+					// Add meta with the image count.
+					update_post_meta( $post_id, '_pmc_image_counts', $image_count );
 
-                    if (!in_array('rollingstone', $categories, true)) {
-                        $current_categories = wp_get_post_categories($post_id, array('fields' => 'ids'));
+					if ( ! in_array( 'rollingstone', $categories, true ) ) {
+						$current_categories = wp_get_post_categories( $post_id, array( 'fields' => 'ids' ) );
 
-                        // Remove existing categories.
-                        wp_remove_object_terms($post_id, $current_categories, 'category');
-                        // Assign 'rollingstone' category under 'pmc'.
-                        wp_set_post_categories($post_id, array($child_category['term_id']), true);
-                    }
-                    $count++;
-                }
+						// Remove existing categories.
+						wp_remove_object_terms( $post_id, $current_categories, 'category' );
+						// Assign 'rollingstone' category under 'pmc'.
+						wp_set_post_categories( $post_id, array( $child_category['term_id'] ), true );
+					}
+					++$count;
+				}
 
-                // Free up memory.
-                $this->vip_inmemory_cleanup();
+				// Free up memory.
+				$this->vip_inmemory_cleanup();
 
-                $paged++;
-            } while ($posts_query->have_posts());
+				++$paged;
+			} while ( $posts_query->have_posts() );
 
-            \WP_CLI::success(sprintf('Child category "rollingstone" assigned to %d posts.', $count));
+			\WP_CLI::success( sprintf( 'Child category "rollingstone" assigned to %d posts.', $count ) );
 
-            $this->end_bulk_operation();
-        }
-    }
+			$this->end_bulk_operation();
+		}
+	}
 }
-
--- inc/classes/class-plugin.php
+++ inc/classes/class-plugin.php
@@ -5,8 +5,8 @@ namespace PMC_Plugin\Inc\Classes\Class_Plugin;
 /**
  * Plugin
  */
-class Plugin
-{
+class Plugin {
+
 
 
 	/**
@@ -21,12 +21,11 @@ class Plugin
 	 *
 	 * @return object Instance of current class.
 	 */
-	public static function get_instance()
-	{
+	public static function get_instance() {
 
 		static $instance = false;
 
-		if (false === $instance) {
+		if ( false === $instance ) {
 			$instance = new self();
 		}
 		return $instance;
@@ -37,9 +36,8 @@ class Plugin
 	 *
 	 * This will register commands for assigning categories.
 	 */
-	protected function __construct()
-	{
-		\WP_CLI::add_command('assign-category', 'PMC_Plugin\Inc\Classes\Class_Assign_Category\Assign_Category');
+	protected function __construct() {
+		\WP_CLI::add_command( 'assign-category', 'PMC_Plugin\Inc\Classes\Class_Assign_Category\Assign_Category' );
 	}
 
 	/**
@@ -48,21 +46,39 @@ class Plugin
 	 * @param int $post_id Post ID.
 	 * @return int Image count.
 	 */
-	public static function count_images(int $post_id): int {
+	public static function count_images( int $post_id ): int {
 
 		// Get featured image count.
-		$featured_image_count = has_post_thumbnail($post_id) ? 1 : 0;
-	
+		$featured_image_count = has_post_thumbnail( $post_id ) ? 1 : 0;
+
 		// Get post content.
-		$post_content = get_post_field('post_content', $post_id);
-	
+		$post_content = get_post_field( 'post_content', $post_id );
+
 		// Count images in post content.
-		preg_match_all('/<img[^>]+>/', $post_content, $img_matches);
-		$img_count = count($img_matches[0]);
-	
+		preg_match_all( '/<img[^>]+>/', $post_content, $img_matches );
+		$img_count = count( $img_matches[0] );
+
+		$image_count_shortcode = 0;
+		// If post content has gallery shortcode.
+		if ( has_shortcode( $post_content, 'gallery' ) ) {
+			// Fetching all the shortcodes in the post content.
+			if ( preg_match_all( '/' . get_shortcode_regex() . '/s', $post_content, $matches, PREG_SET_ORDER ) ) {
+				foreach ( $matches as $shortcode ) {
+					if ( 'gallery' === $shortcode[2] ) {
+						$gallery = do_shortcode_tag( $shortcode );
+						// Fetching src from all the images in the gallery shorcode.
+						preg_match_all( '#src=([\'"])(.+?)\1#is', $gallery, $src, PREG_SET_ORDER );
+						if ( ! empty( $src ) ) {
+							$image_count_shortcode += count( $src );
+						}
+					}
+				}
+			}
+		}
+
 		// Total image count.
-		$total_image_count = $featured_image_count + $img_count;
-	
+		$total_image_count = $featured_image_count + $img_count + $image_count_shortcode;
+
 		return $total_image_count;
-	}	
+	}
 }
